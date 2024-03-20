a2beeb422914b2fb45c86b2d59bed7211e38a4e7
Dmitry Bragin <dnbragin@outlook.com>
2024-03-20 22:31:46 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Bragin on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
